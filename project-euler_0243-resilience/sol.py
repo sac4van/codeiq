cf8b4e14c2711abcca847f26e9bc796aaa91659d
sac4van <taka1stdec@gmail.com>
2017-08-27 04:05:00 +0900

    project euler #0243 start

---
+++ project-euler_0243-resilience/sol.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    problem:
+        https://projecteuler.net/problem=243
+"""
+
+from __future__ import print_function
+
+
+def solve():
+    """
+    main function.
+
+    Arguments:
+    """
+
+    ## input validation
+
+
+    ## main
+
+
+# call
+print (solve())
