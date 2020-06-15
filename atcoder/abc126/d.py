1c1898b80a951d7d853cff35c427493127408b5c
sac4van <sac4van@local>
2020-06-16 04:16:13 +0900

    update

--- atcoder/abc126/d.py
+++ atcoder/abc126/d.py
@@ -1,7 +1,7 @@
 import sys
 from collections import defaultdict
 readline = sys.stdin.buffer.readline
-# sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**8)
 
 
 def geta(fn=lambda s: s.decode()):
@@ -13,15 +13,31 @@ def gete(fn=lambda s: s.decode()):
 
 
 def main():
-    n, k = geta(int)
-    ans = 0
-    for i in range(1, n+1):
-        l = i
-        while l < k:
-            l *= 2
-        ans += i/l/n
+    N = gete(int)
+    g = [set() for _ in range(N+1)]
+    for _ in range(N-1):
+        u, v, w = geta(int)
+        g[u].add((v, w))
+        g[v].add((u, w))
 
-    print(ans)
+    dist = [-1] * (N+1)
+    color = [-1]*(N+1)
+
+    dist[1] = 0
+    color[1] = 0
+
+    def dfs(i, d):
+
+        dist[i] = d
+        color[i] = 0 if d & 1 == 0 else 1
+
+        for j, wj in g[i]:
+            if dist[j] < 0:
+                dfs(j, d + wj)
+
+    dfs(1, 0)
+
+    print(*color[1:], sep="\n")
 
 
 if __name__ == "__main__":
